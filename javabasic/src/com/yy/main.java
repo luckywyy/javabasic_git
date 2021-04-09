50b170a1ac3921784d98193d9126e30c09866b98
luckywyy <862024320@qq.com>
2021-04-09 11:04:29 +0800

    添加多态示例代码

---
+++ javabasic/src/com/yy/main.java
@@ -0,0 +1,9 @@
+package com.yy;
+
+public class main {
+    public static void main(String[] args) {
+
+
+        System.out.println("main");
+    }
+}
---
+++ javabasic/src/com/yy/多态例子/Student.java
@@ -0,0 +1,6 @@
+package com.yy.多态例子;
+
+public class Student extends Person{
+
+    
+}
