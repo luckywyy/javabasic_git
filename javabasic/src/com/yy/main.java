05ba517c02c76e172e5c117fe36a240ec6224819
luckywyy <862024320@qq.com>
2021-04-09 16:25:47 +0800

    多态例子

--- javabasic/src/com/yy/main.java
+++ javabasic/src/com/yy/main.java
@@ -1,12 +1,21 @@
 package com.yy;
 
+import com.yy.多态例子.Person;
+import com.yy.多态例子.Student;
+import com.yy.多态例子.Teacher;
+
 public class main {
     public static void main(String[] args) {
 
-
         System.out.println("main");
 
+        Student ob = new Student();
 
-        
+        System.out.println(ob instanceof Object);
+        System.out.println(ob instanceof Person);
+        System.out.println(ob instanceof Student);
+//        System.out.println(ob instanceof Teacher);
+//        System.out.println(ob instanceof String);
+
     }
 }
