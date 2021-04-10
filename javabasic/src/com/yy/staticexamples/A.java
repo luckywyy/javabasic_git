52c57ffa72907c8bdc7bd86dc0dc19f7fa31429b
luckywyy <862024320@qq.com>
2021-04-10 10:39:58 +0800

    添加static例子

--- javabasic/src/com/yy/staticexamples/A.java
+++ javabasic/src/com/yy/staticexamples/A.java
@@ -3,6 +3,12 @@ package com.yy.staticexamples;
 public class A {
 
     public static void main(String[] args) {
-        System.out.println("A");
+//        System.out.println("A");
+
+
+        B b = new B();
+        System.out.println("=====================");
+        B b1 = new B();
+
     }
 }
