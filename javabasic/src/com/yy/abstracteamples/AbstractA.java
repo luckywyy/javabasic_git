a6c19dabdb02f3a4f83782dfe2d6382a4497e201
luckywyy <862024320@qq.com>
2021-04-10 23:46:36 +0800

    添加abstract例子

---
+++ javabasic/src/com/yy/abstracteamples/AbstractA.java
@@ -0,0 +1,19 @@
+package com.yy.abstracteamples;
+
+public abstract class AbstractA {
+
+    public AbstractA(){
+        System.out.println("抽象类的无参构造");
+    }
+
+    public abstract void run();
+
+    public void eat(){
+        System.out.println("抽象类的eat方法");
+    }
+
+    public String getName(int id){
+        return "张三";
+    }
+
+}
