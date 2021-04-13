597bfb74c968d6ebf87a537b719f3f1824e41186
luckywyy <862024320@qq.com>
2021-04-13 10:51:38 +0800

    异常

---
+++ javabasic/src/com/yy/ExceptionExamples/B.java
@@ -0,0 +1,12 @@
+package com.yy.ExceptionExamples;
+
+public class B {
+
+    public static void main(String[] args) {
+
+
+        int[] a = {1, 2, 3};
+
+        System.out.println(a[4]);
+    }
+}
---
+++ javabasic/src/com/yy/ExceptionExamples/D.java
@@ -0,0 +1,10 @@
+package com.yy.ExceptionExamples;
+
+public class D {
+
+    public void dmethod() throws CustomException{
+
+        System.out.println("这是D类的方法，此方法会抛出异常");
+        throw new CustomException("自定义异常");
+    }
+}
