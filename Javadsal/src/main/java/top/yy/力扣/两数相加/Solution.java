7d7de66c50dc84234739e8963028574d0227d470
luckywyy <862024320@qq.com>
2021-12-02 14:44:01 +0800

    Create Solution.java

---
+++ Javadsal/src/main/java/top/yy/力扣/两数相加/Solution.java
@@ -0,0 +1,29 @@
+package top.yy.力扣.两数相加;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+
+        return new ListNode();
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
