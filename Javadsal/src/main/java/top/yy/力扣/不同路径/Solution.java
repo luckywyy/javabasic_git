1a8aa48f058cae2bd1a1f7a1d7edbe293705ef72
luckywyy <862024320@qq.com>
2021-12-13 10:01:52 +0800

    Update Solution.java

--- Javadsal/src/main/java/top/yy/力扣/不同路径/Solution.java
+++ Javadsal/src/main/java/top/yy/力扣/不同路径/Solution.java
@@ -25,6 +25,6 @@ class Solution {
 
     public static void main(String[] args) {
 
-        System.out.println(new Solution().uniquePaths(3, 7));
+        System.out.println(new Solution().uniquePaths(1, 1));
     }
 }
