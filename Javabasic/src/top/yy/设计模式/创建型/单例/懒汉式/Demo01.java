bf6713b8919792fa13acf61319de38c41a03c069
luckywyy <862024320@qq.com>
2021-12-15 10:31:21 +0800

    Update Demo01.java

--- Javabasic/src/top/yy/设计模式/创建型/单例/懒汉式/Demo01.java
+++ Javabasic/src/top/yy/设计模式/创建型/单例/懒汉式/Demo01.java
@@ -5,6 +5,7 @@ package top.yy.设计模式.创建型.单例.懒汉式;
  * 懒汉式即不使用时不创建，只有调用后才创建
  * 但是这里式线程不安全的，因为可能有多个线程判断是否为空
  * 但是并没有执行到下面创建对象的语句
+ * 解决方法可以加同步锁 syn关键字等
  */
 public class Demo01 {
     private Demo01 () {}
@@ -17,4 +18,11 @@ public class Demo01 {
         }
         return instance;
     }
+
+//    public static synchronized Demo01 getInstance() {
+//        if (instance == null) {
+//            instance = new Demo01();
+//        }
+//        return instance;
+//    }
 }
