407d2c209a6946afff3e3cb89e558cf449a6ba1e
luckywyy <862024320@qq.com>
2021-12-15 10:19:25 +0800

    Update Demo01.java

--- Javabasic/src/top/yy/设计模式/创建型/单例/饿汉式/Demo01.java
+++ Javabasic/src/top/yy/设计模式/创建型/单例/饿汉式/Demo01.java
@@ -4,7 +4,7 @@ public class Demo01 {
 
     private Demo01() {}
     private static Demo01 INSTANCE = new Demo01();
-    public static Demo01 getINSTANCE() {
+    public static Demo01 getInstance() {
         return INSTANCE;
     }
 }
