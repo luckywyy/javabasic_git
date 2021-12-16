86900d1e25d3f62561475717cddcf39b9ce352e6
luckywyy <862024320@qq.com>
2021-12-16 09:28:03 +0800

    Update

--- Javadsal/src/main/java/top/yy/力扣/两个数组的交集II/Solution.java
+++ Javadsal/src/main/java/top/yy/力扣/两个数组的交集II/Solution.java
@@ -1,12 +1,35 @@
 package top.yy.力扣.两个数组的交集II;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
 
-
-
-
-        return new int[]{};
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int num : nums1) {
+            int count = map.getOrDefault(num, 0) + 1;
+            map.put(num, count);
+        }
+        int[] intersection = new int[nums1.length];
+        int index = 0;
+        for (int num : nums2) {
+            int count = map.getOrDefault(num, 0);
+            if (count > 0) {
+                intersection[index++] = num;
+                count--;
+                if (count > 0) {
+                    map.put(num, count);
+                } else {
+                    map.remove(num);
+                }
+            }
+        }
+        return Arrays.copyOfRange(intersection, 0, index);
     }
 
     public static void main(String[] args) {
