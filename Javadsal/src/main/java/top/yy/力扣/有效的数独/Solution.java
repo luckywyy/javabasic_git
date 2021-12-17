0f9392c367c03cc4ef4d377641fdb6f779eb42c0
luckywyy <862024320@qq.com>
2021-12-17 09:13:57 +0800

    Create Solution.java

---
+++ Javadsal/src/main/java/top/yy/力扣/有效的数独/Solution.java
@@ -0,0 +1,19 @@
+package top.yy.力扣.有效的数独;
+
+/**
+ * @program: javabasic_git
+ * @description:
+ * @author:
+ * @create: 2021-12-17 09:12
+ **/
+public class Solution {
+    public boolean isValidSudoku(char[][] board) {
+
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
