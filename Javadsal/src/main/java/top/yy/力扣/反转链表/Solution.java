75457f8de015c536e2a4c349be46ea720c83504c
luckywyy <862024320@qq.com>
2021-12-21 20:32:41 +0800

    Create Solution.java

---
+++ Javadsal/src/main/java/top/yy/力扣/反转链表/Solution.java
@@ -0,0 +1,47 @@
+package top.yy.力扣.反转链表;
+
+/**
+ * @program: javabasic_git-top.yy.力扣.反转链表
+ * @description:
+ * @author:
+ * @create: 2021-12-21 20:31
+ **/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
