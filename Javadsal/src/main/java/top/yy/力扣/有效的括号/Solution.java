508087659fffc07232cba1287ee4895796f42a2c
luckywyy <862024320@qq.com>
2021-12-21 19:59:57 +0800

    Update Solution.java

--- Javadsal/src/main/java/top/yy/力扣/有效的括号/Solution.java
+++ Javadsal/src/main/java/top/yy/力扣/有效的括号/Solution.java
@@ -6,5 +6,19 @@ package top.yy.力扣.有效的括号;
  * @author:
  * @create: 2021-12-21 19:57
  **/
-public class Solution {
+class Solution {
+    public boolean isValid(String s) {
+        
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String s = "()[]{}";
+        boolean valid = new Solution().isValid(s);
+        System.out.println(valid);
+    }
 }
