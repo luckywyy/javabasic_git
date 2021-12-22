9c4520e44d8987bc018ba725bf701fbb4422ba0c
luckywyy <862024320@qq.com>
2021-12-22 09:01:18 +0800

    Update Solution.java

--- Javadsal/src/main/java/top/yy/力扣/反转链表/Solution.java
+++ Javadsal/src/main/java/top/yy/力扣/反转链表/Solution.java
@@ -18,13 +18,25 @@ package top.yy.力扣.反转链表;
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        while (head.next != null) {
-            ListNode temp = new ListNode(0);
-            head.next = temp;
-            temp.next = head.next;
-            head.
+//        ListNode prev = null;
+//        ListNode curr = head;
+//        while (curr != null) {
+//            ListNode next = curr.next;
+//            curr.next = prev;
+//            prev = curr;
+//            curr = next;
+//        }
+//        return prev;
+
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
         }
-        return null;
+        return prev;
     }
 
     public static void main(String[] args) {
