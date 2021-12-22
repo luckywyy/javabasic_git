68433dcffb8a21b9595a242009a1fc723a9d9041
luckywyy <862024320@qq.com>
2021-12-22 15:22:48 +0800

    Create Solution.java

---
+++ Javadsal/src/main/java/top/yy/力扣/二叉树的遍历/层序遍历/Solution.java
@@ -0,0 +1,50 @@
+package top.yy.力扣.二叉树的遍历.层序遍历;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+
+        return new ArrayList<>(new ArrayList<>());
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
