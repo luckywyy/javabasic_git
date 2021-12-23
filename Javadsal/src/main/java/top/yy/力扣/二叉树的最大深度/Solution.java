cf43b54608770a039cfde7bf6e7c905988327c93
luckywyy <862024320@qq.com>
2021-12-23 10:25:37 +0800

    Update Solution.java

--- Javadsal/src/main/java/top/yy/力扣/二叉树的最大深度/Solution.java
+++ Javadsal/src/main/java/top/yy/力扣/二叉树的最大深度/Solution.java
@@ -20,12 +20,25 @@ class Solution {
     public int maxDepth1(TreeNode root) {
         return 0;
     }
-//    解法2 深度优先
+//    解法2 深度优先 走遍每一条路径
+    int depth = 0;
+//    int depth = Integer.MIN_VALUE;
     public int maxDepth2(TreeNode root) {
 
-        
+        DLR(root, 0);
 
-        return 0;
+        return depth;
+    }
+    public void DLR(TreeNode node, int depth) {
+        if (node == null) {
+            return;
+        }
+        depth += 1;
+        if (depth > this.depth) {
+            this.depth = depth;
+        }
+        DLR(node.left, depth);
+        DLR(node.right, depth);
     }
 
 
