609ac20a3df11c4a285b9df08c34b6b2c08a928a
luckywyy <862024320@qq.com>
2021-12-23 10:29:05 +0800

    Create MyQueue.java

---
+++ Javadsal/src/main/java/top/yy/力扣/用栈实现队列/MyQueue.java
@@ -0,0 +1,51 @@
+package top.yy.力扣.用栈实现队列;
+
+/**
+ * @program: javabasic_git-top.yy.力扣.用栈实现队列
+ * @description:
+ * 请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）：
+ *
+ * 实现 MyQueue 类：
+ *
+ * void push(int x) 将元素 x 推到队列的末尾
+ * int pop() 从队列的开头移除并返回元素
+ * int peek() 返回队列开头的元素
+ * boolean empty() 如果队列为空，返回 true ；否则，返回 false
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/implement-queue-using-stacks
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author:
+ * @create: 2021-12-23 10:27
+ **/
+class MyQueue {
+
+    public MyQueue() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public int pop() {
+
+    }
+
+    public int peek() {
+
+    }
+
+    public boolean empty() {
+
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
