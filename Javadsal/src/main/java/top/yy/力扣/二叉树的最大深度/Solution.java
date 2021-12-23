ffa5404dedbfbba3af4f83cc1d74dae43d83586e
luckywyy <862024320@qq.com>
2021-12-23 10:14:45 +0800

    Create Solution.java

---
+++ Javadsal/src/main/java/top/yy/力扣/二叉树的最大深度/Solution.java
@@ -0,0 +1,55 @@
+package top.yy.力扣.二叉树的最大深度;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+//    解法1 广度优先 有多少层有多深
+    public int maxDepth1(TreeNode root) {
+        return 0;
+    }
+//    解法2 深度优先
+    public int maxDepth2(TreeNode root) {
+
+        
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
