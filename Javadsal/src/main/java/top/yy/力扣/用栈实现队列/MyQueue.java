d5430dbe046e7dc43d24ee734ccbeba495a6e497
luckywyy <862024320@qq.com>
2021-12-23 10:30:49 +0800

    Update MyQueue.java

--- Javadsal/src/main/java/top/yy/力扣/用栈实现队列/MyQueue.java
+++ Javadsal/src/main/java/top/yy/力扣/用栈实现队列/MyQueue.java
@@ -1,5 +1,8 @@
 package top.yy.力扣.用栈实现队列;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @program: javabasic_git-top.yy.力扣.用栈实现队列
  * @description:
@@ -19,27 +22,43 @@ package top.yy.力扣.用栈实现队列;
  * @create: 2021-12-23 10:27
  **/
 class MyQueue {
+    Deque<Integer> inStack;
+    Deque<Integer> outStack;
 
     public MyQueue() {
-
+        inStack = new LinkedList<Integer>();
+        outStack = new LinkedList<Integer>();
     }
 
     public void push(int x) {
-
+        inStack.push(x);
     }
 
     public int pop() {
-
+        if (outStack.isEmpty()) {
+            in2out();
+        }
+        return outStack.pop();
     }
 
     public int peek() {
-
+        if (outStack.isEmpty()) {
+            in2out();
+        }
+        return outStack.peek();
     }
 
     public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
 
+    private void in2out() {
+        while (!inStack.isEmpty()) {
+            outStack.push(inStack.pop());
+        }
     }
 }
+
 
 /**
  * Your MyQueue object will be instantiated and called as such:
