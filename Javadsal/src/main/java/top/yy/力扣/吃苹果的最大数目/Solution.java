cdc325f518294c050b502fb05d94584cb0b7a557
luckywyy <862024320@qq.com>
2021-12-24 13:55:49 +0800

    Update Solution.java

--- Javadsal/src/main/java/top/yy/力扣/吃苹果的最大数目/Solution.java
+++ Javadsal/src/main/java/top/yy/力扣/吃苹果的最大数目/Solution.java
@@ -1,5 +1,7 @@
 package top.yy.力扣.吃苹果的最大数目;
 
+import java.util.PriorityQueue;
+
 /**
  * @program: javabasic_git-top.yy.力扣.吃苹果的最大数目
  * @description:
@@ -8,7 +10,45 @@ package top.yy.力扣.吃苹果的最大数目;
  **/
 public class Solution {
 
-    
+    public int eatenApples(int[] apples, int[] days) {
+        int ans = 0;
+        //
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> a[0] - b[0]);
+        int n = apples.length;
+        int i = 0;
+        while (i < n) {
+            while (!pq.isEmpty() && pq.peek()[0] <= i) {
+                pq.poll();
+            }
+            int rottenDay = i + days[i];
+            int count = apples[i];
+            if (count > 0) {
+                pq.offer(new int[]{rottenDay, count});
+            }
+            if (!pq.isEmpty()) {
+                int[] arr = pq.peek();
+                arr[1]--;
+                if (arr[1] == 0) {
+                    pq.poll();
+                }
+                ans++;
+            }
+            i++;
+        }
+        while (!pq.isEmpty()) {
+            while (!pq.isEmpty() && pq.peek()[0] <= i) {
+                pq.poll();
+            }
+            if (pq.isEmpty()) {
+                break;
+            }
+            int[] arr = pq.poll();
+            int curr = Math.min(arr[0] - i, arr[1]);
+            ans += curr;
+            i += curr;
+        }
+        return ans;
+    }
 
     public static void main(String[] args) {
 
