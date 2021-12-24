4d455188a439bc06e9a0a008c3986def1601b13a
luckywyy <862024320@qq.com>
2021-12-24 13:58:37 +0800

    Create Solution.java

---
+++ Javadsal/src/main/java/top/yy/力扣/简化路径/Solution.java
@@ -0,0 +1,19 @@
+package top.yy.力扣.简化路径;
+
+/**
+ * @program: javabasic_git-top.yy.力扣.简化路径
+ * @description:
+ * @author:
+ * @create: 2021-12-24 13:57
+ **/
+public class Solution {
+    
+    public String simplifyPath(String path) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
