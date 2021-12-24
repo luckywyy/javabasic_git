bb0860dd14b9e96bf923299e3d04c1d679e741c6
luckywyy <862024320@qq.com>
2021-12-24 13:53:49 +0800

    Update Solution.java

--- Javadsal/src/main/java/top/yy/力扣/吃苹果的最大数目/Solution.java
+++ Javadsal/src/main/java/top/yy/力扣/吃苹果的最大数目/Solution.java
@@ -4,7 +4,13 @@ package top.yy.力扣.吃苹果的最大数目;
  * @program: javabasic_git-top.yy.力扣.吃苹果的最大数目
  * @description:
  * @author:
- * @create: 2021-12-24 10:07
+ * @create: 2021-12-24 13:53
  **/
 public class Solution {
+
+    
+
+    public static void main(String[] args) {
+
+    }
 }
