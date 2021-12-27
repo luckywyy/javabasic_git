999109f8080d8704f3264f81f0a8c0bbd4a34132
luckywyy <862024320@qq.com>
2021-12-27 19:02:54 +0800

    Create Solution.java

---
+++ Javadsal/src/main/java/top/yy/力扣/适龄的朋友/Solution.java
@@ -0,0 +1,13 @@
+package top.yy.力扣.适龄的朋友;
+
+/**
+ * @program: javabasic_git-top.yy.力扣.适龄的朋友
+ * @description:
+ * @author:
+ * @create: 2021-12-27 19:02
+ **/
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
