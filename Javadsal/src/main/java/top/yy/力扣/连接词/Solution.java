730b032af6c87c20045ccbbbaedf8a4cd7af1d0c
luckywyy <862024320@qq.com>
2021-12-28 09:19:01 +0800

    Create Solution.java

---
+++ Javadsal/src/main/java/top/yy/力扣/连接词/Solution.java
@@ -0,0 +1,13 @@
+package top.yy.力扣.连接词;
+
+/**
+ * @program: javabasic_git-top.yy.力扣.连接词
+ * @description:
+ * @author:
+ * @create: 2021-12-28 09:18
+ **/
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
