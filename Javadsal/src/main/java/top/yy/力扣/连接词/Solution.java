fd2c9fc3d9a09651bbc4e1ab9ef840f72cc0cff7
luckywyy <862024320@qq.com>
2021-12-28 09:20:32 +0800

    Update Solution.java

--- Javadsal/src/main/java/top/yy/力扣/连接词/Solution.java
+++ Javadsal/src/main/java/top/yy/力扣/连接词/Solution.java
@@ -1,12 +1,20 @@
 package top.yy.力扣.连接词;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @program: javabasic_git-top.yy.力扣.连接词
- * @description:
+ * @description:words = ["cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"]
+ *
  * @author:
  * @create: 2021-12-28 09:18
  **/
 public class Solution {
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+
+        return new ArrayList<String>();
+    }
     public static void main(String[] args) {
 
     }
