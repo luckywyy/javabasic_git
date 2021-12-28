d1ef37fd020db626628eb987b2048d5db81aa8bd
luckywyy <862024320@qq.com>
2021-12-28 20:25:44 +0800

    Create Solution.java

---
+++ Javadsal/src/main/java/top/yy/力扣/第一个错误的版本/Solution.java
@@ -0,0 +1,16 @@
+package top.yy.力扣.第一个错误的版本;
+
+/**
+ * @program: javabasic_git-top.yy.力扣.第一个错误的版本
+ * @description:
+ * @author:
+ * @create: 2021-12-28 19:41
+ **/
+public class Solution {
+    public int firstBadVersion(int n) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
