034b995f6114cfa05a9636de10a27bf2271f489b
luckywyy <862024320@qq.com>
2021-12-31 14:24:08 +0800

    Update Solution.java

--- Javadsal/src/main/java/top/yy/力扣/第一个错误的版本/Solution.java
+++ Javadsal/src/main/java/top/yy/力扣/第一个错误的版本/Solution.java
@@ -8,16 +8,66 @@ package top.yy.力扣.第一个错误的版本;
  **/
 public class Solution {
     public int firstBadVersion(int n) {
-        return -1;
+        if (n==1){
+            return 1;
+        }
+        int left = 1;
+        int right = n;
+        int mid = (left + right) / 2;
+        while (true) {
+            if (mid == 1 && isBadVersion(mid)) break;
+            if (isBadVersion(mid) && !isBadVersion(mid-1)) break;
+            if (isBadVersion(mid)) {
+                right = mid;
+                mid = (left + right) / 2;
+            }
+            if (!isBadVersion(mid)) {
+                left = mid;
+                mid = (left + right) / 2;
+            }
+        }
+        return mid;
     }
     public boolean isBadVersion(int v) {
-        if (v==4){
+        if (v>=112312312){
             return true;
         } else {
             return false;
         }
     }
-    public static void main(String[] args) {
 
+    public int firstBadVersion2(int n) {
+        if (n==1){
+            return 1;
+        }
+        int ret = -1;
+        for (int i = 1; i <= n; i++) {
+            if (isBadVersion(i) && !isBadVersion(i-1)) {
+                ret = i;
+                break;
+            }
+        }
+        return ret;
+    }
+
+    public int firstBadVersion3(int n) {
+        if (n==1){
+            return 1;
+        }
+        int ret = -1;
+        for (int i = 1; i <= n; i++) {
+            if (!isBadVersion((i+n)/2)) {
+                i = (i+n)/2;
+                continue;
+            }
+            if (isBadVersion(i) && !isBadVersion(i-1)) {
+                ret = i;
+                break;
+            }
+        }
+        return ret;
+    }
+    public static void main(String[] args) {
+        System.out.println(new Solution().firstBadVersion(412312312));
     }
 }
