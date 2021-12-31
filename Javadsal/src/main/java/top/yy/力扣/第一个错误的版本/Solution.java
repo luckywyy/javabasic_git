5bcdaf4506320ecb9467fccdadfeb894f6cbc885
luckywyy <862024320@qq.com>
2021-12-31 14:38:37 +0800

    Update Solution.java

--- Javadsal/src/main/java/top/yy/力扣/第一个错误的版本/Solution.java
+++ Javadsal/src/main/java/top/yy/力扣/第一个错误的版本/Solution.java
@@ -67,6 +67,20 @@ public class Solution {
         }
         return ret;
     }
+    public int firstBadVersion4(int n) {
+        int left = 1, right = n;
+        while (left < right) { // 循环直至区间左右端点相同
+            int mid = left + (right - left) / 2; // 防止计算时溢出
+            if (isBadVersion(mid)) {
+                right = mid; // 答案在区间 [left, mid] 中
+            } else {
+                left = mid + 1; // 答案在区间 [mid+1, right] 中
+            }
+        }
+        // 此时有 left == right，区间缩为一个点，即为答案
+        return left;
+    }
+
     public static void main(String[] args) {
         System.out.println(new Solution().firstBadVersion(412312312));
     }
